c50efc2d1910339ae69aef89825c66ab2bef6099
Dave DiGioia <dejoya@gmail.com>
2013-12-02 16:13:52 -0800

    set up "different" section

--- public/js/home.js
+++ public/js/home.js
@@ -24,12 +24,21 @@
             }
         });
 
-        $("#hero .owl-carousel").owlCarousel({
+        $('#hero .owl-carousel').owlCarousel({
             slideSpeed : 300,
             paginationSpeed : 400,
             singleItem:true,
             pagination: true
+        });
 
+        $('#how-were-different .owl-carousel').owlCarousel({
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            singleItem:true,
+            pagination: false,
+            navigation: true,
+            navigationText: false,
+            rewindNav: false
         });
 
     });
