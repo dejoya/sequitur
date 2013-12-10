5e07913f449f91dfbd1493f51a0bfab9acef35e6
Dave DiGioia <dejoya@gmail.com>
2013-12-09 21:28:40 -0800

    add view all functionality

--- public/js/home.js
+++ public/js/home.js
@@ -41,6 +41,11 @@
             rewindNav: false
         });
 
+        $('#what-we-do .view-all').on('click', function(){
+            $('#what-we-do .box-grid .hidden').removeClass('hidden');
+            $(this).addClass('hidden');
+        });
+
     });
 
 }(jQuery));
